678b8da21d6ab4cba5375dd8dfd7779f5764e7a7
Isa Alkhanous <programmingquack@gmail.com>
2024-11-18 20:35:38 +0400

    changes to many pages
    -added 3d model
    -changed the homepage
    -changed the header and footer, added them to main js file
    -created eyestyle (not done)
    -changed old index.html to entities.html
    -added effects for app001
    -replaced header and footer for the files

---
+++ js/main.js
@@ -0,0 +1,57 @@
+class myHeader extends HTMLElement{
+  connectedCallback() {
+    this.innerHTML = `
+    <header>
+      <div class="header-top">
+        <div class="logo">
+          <a href="../index.html">
+            <img src="../images/app.svg" alt="APP logo" style="width:80px; mix-blend-mode: multiply;">
+            A.P.P Foundation
+          </a>
+        </div>
+        <a href="LogIn_page.html" id="login">Personnel Access</a>
+      </div>
+      <div id="bar">
+      <a href="entities.html">Entities</a>&emsp;
+        <a href="about.html">About</a>&emsp;
+        <a href="community.html">Community</a>&emsp;
+        <a href="resources.html">Resources</a>&emsp;
+        <a href="sister_sites.html">Sister Sites</a>&emsp;
+        <a href="contact.html">Contact Us</a>&emsp;
+      </div>
+    </header> `
+  }
+}
+class myFooter extends HTMLElement{
+  connectedCallback() {
+    this.innerHTML = `
+     <footer>
+      <hr> <!--Thematic break-->
+      Author: GRP_11
+      &copy; copyrights reserved
+      <small><a href="mailto:email@gmail.com">TheFake@email.com</small>
+  </footer>
+    `
+  }
+}
+
+customElements.define('my-header', myHeader)
+customElements.define('my-footer', myFooter)
+
+function scrollingHeader() {
+  let lastScrollY = window.scrollY;
+  const header = document.querySelector('header');
+
+  window.addEventListener('scroll', () => {
+      if (window.scrollY > lastScrollY) {
+          // Scrolling down
+          header.classList.add('header-hidden');
+      } else {
+          // Scrolling up
+          header.classList.remove('header-hidden');
+      }
+      lastScrollY = window.scrollY;
+  });
+}
+
+window.onload = scrollingHeader()
