6ccbc4ad45c0afbc521d67f29d73ac4eb6b9dd7c
Isa Alkhanous <programmingquack@gmail.com>
2024-11-19 10:27:42 +0400

    small header tweaks for hovering

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ class myHeader extends HTMLElement{
             A.P.P Foundation
           </a>
         </div>
-        <a href="../LogIn_page.html" id="login">Personnel Access</a>
+          <a href="../LogIn_page.html" id="login">Personnel Access</a>
       </div>
       <div id="bar">
       <a href="entities.html">Entities</a>&emsp;
